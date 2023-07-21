e77a2f335ef567d17d3b51fb344eadcd83540c6e
Wenderson Santos <souowendy@gmail.com>
2023-07-21 14:34:04 -0300

    Create Get CashCard List with Sorting and Pagination

--- src/main/java/br/com/souowendy/cashcard/CashCardController.java
+++ src/main/java/br/com/souowendy/cashcard/CashCardController.java
@@ -1,10 +1,15 @@
 package br.com.souowendy.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -30,5 +35,17 @@ public class CashCardController {
                 .buildAndExpand(savedCashCard.id())
                 .toUri();
         return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = (Page<CashCard>) cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                )
+        );
+        return ResponseEntity.ok(page.getContent());
     }
 }
--- src/main/java/br/com/souowendy/cashcard/CashCardRepository.java
+++ src/main/java/br/com/souowendy/cashcard/CashCardRepository.java
@@ -1,6 +1,7 @@
 package br.com.souowendy.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
